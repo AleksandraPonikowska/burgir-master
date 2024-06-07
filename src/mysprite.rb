056d19886f95f95ff1c26f577cc7aee2c4e3c080
Ola <ponikowska.books@gmail.com>
2024-06-07 12:40:26 +0200

    Deleted unused method

--- src/mysprite.rb
+++ src/mysprite.rb
@@ -1,9 +1,7 @@
-# mysprite.rb
 class MySprite
     attr_reader :x, :y
   
     def initialize(image_path, x, y)
-      @id = "tba"
       @x = x
       @y = y
       @object = Sprite.new("assets/" + image_path, x: @x, y: @y)
@@ -18,14 +16,6 @@ class MySprite
         @y = y
         @object.x = @x
         @object.y = @y
-    end
-
-    def set_img(image_path)
-        fixed_path = "assets/" + image_path
-        if @object.path != fixed_path
-            @object.remove
-            @object = Sprite.new(fixed_path, x: @x, y: @y)
-        end
     end
 
   end
